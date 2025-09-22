f60aec414dacfbdfaec88285f43326bf9404ba3e
Guichard <rene.guichard7@gmail.com>
2025-09-22 14:27:03 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const SerialPort = require('serialport');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Remplace '/dev/ttyUSB0' par le port série de ton Arduino (ex: '/dev/ttyACM0' ou '/dev/ttyUSB0')
+const arduino = new SerialPort.SerialPort({
+    path: '/dev/ttyUSB0',
+    baudRate: 9600
+});
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname)); // Sert Led_Manager.html et les fichiers statiques
+
+app.post('/envoyer', (req, res) => {
+    const pwm = req.body.pwm;
+    if (typeof pwm === 'undefined') {
+        return res.status(400).send('Valeur PWM manquante');
+    }
+    arduino.write(pwm + '\n', (err) => {
+        if (err) {
+            return res.status(500).send('Erreur série');
+        }
+        res.send(pwm.toString());
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
